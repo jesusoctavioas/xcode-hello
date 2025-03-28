d39d95c28f262275a9ca6311359b00334de9f13a
jesus octavio <jesusoctavioas@gmail.com>
2025-03-28 12:50:34 -0700

    downgrade version

---
+++ xcode-hello/xcode-hello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcode-hello
+//
+//  Created by Jesus Octavio Acosta Santos on 28/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
